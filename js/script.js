4480f5ef3480b3e9039a153cba5fcdcf39b3d776
MYarush <yarushmaksim@gmail.com>
2025-03-09 14:20:15 +0100

    adaptive done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+
+(function($) { "use strict";
+	var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();	
+})(jQuery); 
+
 const swiper = new Swiper('.swiper', {
     slidesPerView: 4,
     slidesPerGroup: 1,
@@ -19,4 +43,16 @@ const swiper = new Swiper('.swiper', {
         nextEl: ".custom-next",
         prevEl: ".custom-prev",
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+        },
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 4,
+        }
+      }
   });
